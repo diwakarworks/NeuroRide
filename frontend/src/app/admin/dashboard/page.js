234bbcf90dcedf0f5ac76278a6cf21dfa8fb4092
SPIDY123456 <73572033+SPIDY123456@users.noreply.github.com>
2025-03-13 18:20:57 +0530

    Update page.js

--- frontend/src/app/admin/dashboard/page.js
+++ frontend/src/app/admin/dashboard/page.js
@@ -1,6 +1,6 @@
 "use client";
 import { useEffect, useState } from "react";
-import { fetchDashboardStats } from "@/app/lib/api/fetchDashboardStats";
+import { fetchDashboardStats } from "@/app/lib/api/page";
 import AdminLayout from "@/app/components/AdminLayout";
 import { motion } from "framer-motion";
 
