b0d7a84bced7c4fe2515411ca6874ad469a0a29e
SPIDY123456 <73572033+SPIDY123456@users.noreply.github.com>
2025-03-13 18:52:27 +0530

    Update dashboard.ts

--- frontend/src/app/lib/api/dashboard.ts
+++ frontend/src/app/lib/api/dashboard.ts
@@ -3,7 +3,7 @@
 export const fetchDashboardStats = async () => {
   const API_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
   
-  const res = await fetch(`${API_URL}/api/admin/dashboard-stats`);
+  const res = await fetch(`${API_URL}/api/admin/stats`);
   if (!res.ok) throw new Error("Failed to fetch dashboard stats");
   
   return res.json();
