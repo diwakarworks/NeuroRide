4de22234e1c4b575439a77538196bf2c4023e511
SPIDY123456 <73572033+SPIDY123456@users.noreply.github.com>
2025-03-14 08:44:15 +0530

    Update adminActions.js

--- frontend/src/app/lib/api/adminActions.js
+++ frontend/src/app/lib/api/adminActions.js
@@ -1,16 +1,15 @@
-"use server";
+
 
 const API_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
 
-const getAuthHeaders = () => ({
-  "Content-Type": "application/json",
-  Authorization: `Bearer ${localStorage.getItem("token")}`, 
-});
 
 
-export const fetchUsers = async () => {
+export const fetchUsers = async (token)) => {
   const res = await fetch(`${API_URL}/api/admin/users`, {
-    headers: getAuthHeaders(),  
+     headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,  
+    },  
   });
 
   if (!res.ok) {
@@ -19,10 +18,13 @@ export const fetchUsers = async () => {
   return res.json();
 };
 
-export const toggleUserStatus = async (id) => {
+export const toggleUserStatus = async (id,token) => {
   const res = await fetch(`${API_URL}/api/admin/users/${id}/status`, {
     method: "PATCH",
-   headers: getAuthHeaders(),
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`, 
+    },
   });
 
   if (!res.ok) {
@@ -31,10 +33,13 @@ export const toggleUserStatus = async (id) => {
   return res.json();
 };
 
-export const verifyDriver = async (id) => {
+export const verifyDriver = async (id,token) => {
   const res = await fetch(`${API_URL}/api/admin/verify/${id}`, {
     method: "PATCH",
-    headers: getAuthHeaders(),
+     headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`, 
+    },
   });
 
   return res.json();
