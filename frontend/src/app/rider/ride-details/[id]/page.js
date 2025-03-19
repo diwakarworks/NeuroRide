2a4997e6c205c30ba9e94055d433d15a89f68b04
SPIDY123456 <73572033+SPIDY123456@users.noreply.github.com>
2025-03-19 19:09:16 +0530

    Update page.js

--- frontend/src/app/rider/ride-details/[id]/page.js
+++ frontend/src/app/rider/ride-details/[id]/page.js
@@ -114,7 +114,7 @@ const RideDetails = () => {
 
         const driverId = data.driver._id;
 
-        const socket = new WebSocket(`ws://localhost:8000/ws/track/${driverId}`);
+        const socket = new WebSocket(`ws://https://neuroride-9.onrender.com/ws/track/${driverId}`);
 
         socket.onmessage = (event) => {
           const data = JSON.parse(event.data);
